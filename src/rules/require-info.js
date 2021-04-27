efda49578e4a880732619716007029738cbe721d
freearhey <free.arhey@gmail.com>
2021-04-27 17:54:20 +0300

    Update require-info.js

--- src/rules/require-info.js
+++ src/rules/require-info.js
@@ -2,13 +2,13 @@ module.exports = function (lines) {
   const errors = []
   let prevLinkIndex = 0
   lines.forEach((line, index) => {
-    if (!line.startsWith('#')) {
+    if (line && !line.startsWith('#')) {
       let prevLines = lines.slice(prevLinkIndex, index)
-      if (!prevLines.find(l => l.startsWith('#EXTINF:'))) {
+      if (!prevLines.find(l => l.startsWith('#EXTINF'))) {
         errors.push({
           line: index + 1,
           column: 1,
-          message: `Resource must have a corresponding '#EXTINF:' directive`
+          message: `Resource must have a corresponding '#EXTINF' directive`
         })
       }
       prevLinkIndex = index
