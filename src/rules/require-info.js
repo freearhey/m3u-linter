4c8c545ae36ca914756f07bebb4bc5c84bf72a8c
freearhey <free.arhey@gmail.com>
2021-04-27 16:02:30 +0300

    Update require-info.js

--- src/rules/require-info.js
+++ src/rules/require-info.js
@@ -1,20 +1,17 @@
-const isValidPath = require('is-valid-path')
-const validUrl = require('valid-url')
-
 module.exports = function (lines) {
   const errors = []
+  let prevLinkIndex = 0
   lines.forEach((line, index) => {
-    if (
-      line.trim().length &&
-      (isValidPath(line) || validUrl.isUri(line)) &&
-      lines[index - 1] &&
-      !lines[index - 1].startsWith('#EXTINF:')
-    ) {
-      errors.push({
-        line: index + 1,
-        column: 1,
-        message: `Resource must have a corresponding '#EXTINF:' directive`
-      })
+    if (!line.startsWith('#')) {
+      let prevLines = lines.slice(prevLinkIndex, index)
+      if (!prevLines.find(l => l.startsWith('#EXTINF:'))) {
+        errors.push({
+          line: index + 1,
+          column: 1,
+          message: `Resource must have a corresponding '#EXTINF:' directive`
+        })
+      }
+      prevLinkIndex = index
     }
   })
   return errors
