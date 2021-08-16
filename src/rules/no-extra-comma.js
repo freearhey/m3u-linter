efae73480c098b7839359843fc9a07f62656bd40
Aleksandr Statciuk <free.arhey@gmail.com>
2021-08-16 15:56:57 +0300

    Update no-extra-comma.js

--- src/rules/no-extra-comma.js
+++ src/rules/no-extra-comma.js
@@ -1,8 +1,8 @@
 module.exports = function (lines) {
   const errors = []
   lines.forEach((line, index) => {
-    if (/^(#EXT-X-MEDIA|#EXT-X-STREAM-INF)/.test(line)) return
-    const matches = line.startsWith('#EXTINF') ? line.match(/,.*(?=,)/) : line.match(/,/)
+    if (!/^#EXTINF/.test(line)) return
+    const matches = line.match(/,.*(?=,)/)
     if (matches) {
       errors.push({
         line: index + 1,
