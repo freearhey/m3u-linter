57527ae3fde00a64d7e93a4a47c7ae2f0df83fc9
freearhey <7253922+freearhey@users.noreply.github.com>
2024-12-20 07:08:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const Logger = require('./utils/logger')
 const fs = require('fs')
-const glob = require('glob')
+const { glob } = require('glob')
 
 module.exports = {
   lint(config, files) {
@@ -25,47 +25,47 @@ module.exports = {
     })
   },
   checkFile(filePath, config) {
-    return new Promise(resolve => {
-      glob(filePath, (globError, files) => {
-        if (globError) {
-          Logger.error(globError.toString())
-        } else {
-          let filesRead = 0
-          let lintErrors = 0
-          files.forEach(file => {
-            fs.readFile(file, (fileError, data) => {
-              if (fileError) {
-                Logger.error(fileError.toString())
-              } else {
-                const errors = []
-                const fileString = data.toString()
-                const lines = fileString.split('\n')
+    return new Promise(async resolve => {
+      try {
+        const files = await glob(filePath)
 
-                for (let rule in config.rules) {
-                  if (config.rules[rule]) {
-                    const validate = require(`./rules/${rule}.js`)
-                    validate(lines).forEach(err => {
-                      errors.push({
-                        ...err,
-                        rule
-                      })
+        let filesRead = 0
+        let lintErrors = 0
+        files.forEach(file => {
+          fs.readFile(file, (fileError, data) => {
+            if (fileError) {
+              Logger.error(fileError.toString())
+            } else {
+              const errors = []
+              const fileString = data.toString()
+              const lines = fileString.split('\n')
+
+              for (let rule in config.rules) {
+                if (config.rules[rule]) {
+                  const validate = require(`./rules/${rule}.js`)
+                  validate(lines).forEach(err => {
+                    errors.push({
+                      ...err,
+                      rule
                     })
-                  }
-                }
-
-                if (errors.length > 0) {
-                  lintErrors += errors.length
-                  Logger.results(file, errors)
+                  })
                 }
               }
 
-              if (++filesRead === files.length) {
-                resolve(lintErrors)
+              if (errors.length > 0) {
+                lintErrors += errors.length
+                Logger.results(file, errors)
               }
-            })
+            }
+
+            if (++filesRead === files.length) {
+              resolve(lintErrors)
+            }
           })
-        }
-      })
+        })
+      } catch (err) {
+        Logger.error(err.toString())
+      }
     })
   }
 }
